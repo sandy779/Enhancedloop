4a587b4aad85a9e61d9c70beb0a9ac4fcb042639
sandip ahire <sandipahire34@gmail.com>
2021-09-04 14:05:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enhancedloop
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] fibnum = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+            int count = 0;
+            foreach(int x in fibnum)
+            {
+                count++;
+                Console.WriteLine($"{x}:{count}");
+               
+
+            }
+            Console.WriteLine($"There are {count} numbers");
+
+        }
+    }
+}
